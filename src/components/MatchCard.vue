8ab10c86388d1267f9107ea836f1a023be7b1a2e
Bradley Ellis <bradleyaellis@gmail.com>
2023-12-18 13:07:42 -0600

    update MatchCard.vue

--- src/components/MatchCard.vue
+++ src/components/MatchCard.vue
@@ -1,56 +1,41 @@
 <template>
-  <v-card> 
-    <v-container fluid class="d-flex justify-space-between"> 
-      <div> 
-        <div class="text-caption">Match</div>
-        {{ match.name }}
-      </div>
-      <div> 
-        <div class="text-caption">Stipulation</div>
-        {{ match.stipulation }}
-      </div>
-      <div>
-        <div class="text-caption">Result</div>
-        {{  match.result }}
-      </div>
-      <div>
-        <div class="text-caption">Your Rating</div>
-        <v-slider v-model="rating" :min="0" :max="5" :step="1" show-ticks :disabled="hasVote" :ticks="tickLabels" />
-        <v-btn @click="doVote"> Vote !</v-btn>
-      </div>
-      <div>
-        <v-slider v-model="collectiveRating" :min="0" :max="5" :step="1" show-ticks readonly ticks="tickLabels" />
-      </div>
-    </v-container>
-  </v-card>
+  <v-container fluid> 
+    <v-card class="rounded-0">
+      <v-card-text>
+        <v-row no-gutters>
+          <v-col>
+            <div class="text-caption font-weight-black">Match</div>
+            {{ match.name }}
+          </v-col>
+          <v-col>
+            <div class="text-caption font-weight-black">Stipulation</div>
+            {{ match.stipulation }}
+          </v-col>
+          <v-col>
+            <div class="text-caption font-weight-black">Result</div>
+            {{  match.result }}
+          </v-col>
+          <v-col>
+            <div class="text-caption font-weight-black"> Rating </div>
+              4.5
+          </v-col>
+          <v-col cols="1">
+            <VoteDialog :matchId="match.id" userId="3" :matchName="match.name" />
+          </v-col>
+        </v-row>
+      </v-card-text> 
+    </v-card>
+  </v-container>
 </template>
 
 <script setup> 
-import { supabase } from "../plugins/supabase"
-import { computed, defineProps, ref } from "vue"
+import { defineProps } from "vue"
+import VoteDialog from "./VoteDialog.vue"
 
-const rating = ref(0)
-const existingVote = ref(null)
 const props = defineProps({
   match: Object
 })
 
-const tickLabels = { 1: '1', 2: '2', 3: '3', 4: '4', 5: '5'} 
+console.log("M", props.match )
 
-const doVote = async () => {
-  const { data, error } = await supabase
-  .from('match_votes')
-  .insert({ id: props.match.id, rating: rating.value })
-  .select()  
-
-  if(data) { 
-    existingVote.value = data[0].id
-  } else {
-    console.log(error)
-  }
-}
-
-const hasVote = computed(() => {
-  return existingVote.value ? true : false
-})
 </script>
