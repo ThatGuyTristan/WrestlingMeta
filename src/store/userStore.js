03d15846b9f38ff18f2d25be0642e6d2491626ca
Bradley Ellis <bradleyaellis@gmail.com>
2023-12-19 14:55:50 -0600

    refactor userStore

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,12 +1,65 @@
+import { computed, ref } from "vue"
 import { defineStore } from 'pinia'
-import { ref } from "vue"
+import { supabase } from '../plugins/supabase';
 
 export const useUserStore = defineStore('user',  () => {
-  const admin = ref(true)
-  const toggleAdmin = () => {
-      admin.value = !admin.value }
+  let session = ref({ 
+    access_token: "",
+    expires_at: 0,
+    expires_in: 0,
+    refresh_token: "",
+    token_type: "",
+    user: { }
+  })
 
-  return { admin, toggleAdmin }
+  const isSignedIn = computed(() => {
+    if(!session.value) return false
+    return session.value.access_token ? true : false
+  })
+  
+  const logIn = async (email, password) => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password, 
+    })
+    
+      console.log("doLogin", data)
+    
+      if (data.session) {
+        session.value = data.session
+      } else if (error) { 
+        console.log("logIn:", error)
+        return error
+      }
+  }
+
+  const logOut = async () => {
+    const { error } = await supabase.auth.signOut()
+
+    if (error) {
+      console.log(error)
+    } else {
+      return true
+    }
+  }
+
+  const updateSession = (data) => {
+    session.value = data.session
+  }
+
+  const refresh = async () =>  {
+    const { data, error } = await supabase.auth.refreshSession(session.value.refresh_token)
+    if (error) console.log('Error refreshing session:', error.message)
+    else console.log('Session refreshed successfully:', data)
+  }
+
+  return { 
+    logIn, 
+    logOut, 
+    isSignedIn, 
+    updateSession, 
+    refresh, 
+    session
+  }
 })
 
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrcHJ6Y3Z3ZWZoZGFucmdtY2RjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI4NDM4MTgsImV4cCI6MjAxODQxOTgxOH0.naX17xdgfwDhWB2j_Rq6xsbuvF6Nj_dVMCqcNeFvPJQSUPABASE_URL
