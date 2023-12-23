18fa6ccf5a425c501ab04f11c37f5133bc92bb74
Bradley Ellis <bradleyaellis@gmail.com>
2023-12-23 11:59:11 -0600

    add default theme to vuetify.js

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,5 +11,6 @@ import * as directives from 'vuetify/directives'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify(
   components,
-  directives
+  directives, { 
+  theme: { defaultTheme: 'dark' } }
 )
