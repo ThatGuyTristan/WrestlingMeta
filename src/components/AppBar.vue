0e4949c1bf205eeeaf6ab5f7bf2f619c1a73a13c
Bradley Ellis <bradleyaellis@gmail.com>
2023-12-23 11:57:49 -0600

    rm cl

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,5 +1,5 @@
 <template> 
-  <v-app-bar class="bg-black" elevation="0" >
+  <v-app-bar class="bg-black" elevation="0">
     <v-container class="w-75 mx-auto bg-black ma-0">
       <v-layout class="d-flex justify-end">
       <div v-if="!isSignedIn">
@@ -24,7 +24,6 @@ import { computed } from "vue"
 
 const store = useUserStore();
 const isSignedIn = computed(() => {
-  console.log("????????", store.isSignedIn)
   return store.isSignedIn;
 })
 
